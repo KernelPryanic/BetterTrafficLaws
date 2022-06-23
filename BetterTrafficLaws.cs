041a3dc61993183742d002fe84586dbc5ac0ba09
Kernel Pryanic <kernel.pryanic@protonmail.com>
2022-06-23 22:03:39 +0200

    Refactor and move to ScriptHookVDotNet3

--- BetterTrafficLaws.cs
+++ BetterTrafficLaws.cs
@@ -8,106 +8,105 @@ using NativeUI;
 
 namespace BetterTrafficLaws {
     class BetterTrafficLaws : Script {
-        ScriptSettings config;
-        Keys openMenu;
+        ScriptSettings Config;
+        Keys OpenMenu;
 
-        MenuPool menuPool;
-        UIMenu mainMenu;
-        UIMenuCheckboxItem enabled;
-        UIMenuListItem copsDistance;
-        UIMenuListItem speedUnits;
-        UIMenuListItem speedLimit;
-        UIMenuListItem speedLimitHighway;
-        UIMenuListItem speedFactor;
-        UIMenuListItem starsToAdd;
+        MenuPool MenuPool;
+        UIMenu MainMenu;
+        UIMenuCheckboxItem Enabled;
+        UIMenuListItem CopsDistance;
+        UIMenuListItem SpeedUnits;
+        UIMenuListItem SpeedLimit;
+        UIMenuListItem SpeedLimitHighway;
+        UIMenuListItem SpeedFactor;
+        UIMenuListItem StarsToAdd;
 
-        int redLightCount;
-        int againstTrafficCount;
-        int driveOnPavementCount;
-        int overspeedCount;
-        int overspeedHighwayCount;
-        int period;
+        int RedLightCount;
+        int AgainstTrafficCount;
+        int DriveOnPavementCount;
+        int OverspeedCount;
+        int OverspeedHighwayCount;
+        int Period;
 
         HashSet<int> trafficLights = new HashSet<int> { 1043035044, 862871082, -655644382, -730685616, 656557234, 865627822, 589548997 };
 
         public BetterTrafficLaws() {
-            MainMenu();
+            MainMenuInit();
 
-            config = ScriptSettings.Load(@"./scripts/BetterTrafficLaws.ini");
-            enabled.Checked = (config.GetValue("Configuration", "Enabled", "True") == "True");
-            copsDistance.Index = copsDistance.Items.FindIndex(x => (float) x == config.GetValue("Configuration", "Cops visibility distance", 75f));
-            starsToAdd.Index = starsToAdd.Items.FindIndex(x => (int) x == config.GetValue("Configuration", "Stars to add", 1));
-            speedLimit.Index = speedLimit.Items.FindIndex(x => (float) x == config.GetValue("Configuration", "Speed limit", 40f));
-            speedLimitHighway.Index = speedLimitHighway.Items.FindIndex(x => (float) x == config.GetValue("Configuration", "Speed limit highway", 90f));
-            speedFactor.Index = speedFactor.Items.FindIndex(x => (float) x == config.GetValue("Configuration", "Speed factor", 1f));
-            speedUnits.Index = speedUnits.Items.FindIndex(x => (string) x == config.GetValue("Configuration", "Speed units", "KMH"));
-            openMenu = config.GetValue("Configuration", "Menu key", Keys.B);
+            Config = ScriptSettings.Load(@"./scripts/BetterTrafficLaws.ini");
+            Enabled.Checked = (Config.GetValue("Configuration", "Enabled", "True") == "True");
+            CopsDistance.Index = CopsDistance.Items.FindIndex(x => (float) x == Config.GetValue("Configuration", "CopsVisibilityDistance", 75f));
+            StarsToAdd.Index = StarsToAdd.Items.FindIndex(x => (int) x == Config.GetValue("Configuration", "StarsToAdd", 1));
+            SpeedLimit.Index = SpeedLimit.Items.FindIndex(x => (float) x == Config.GetValue("Configuration", "SpeedLimit", 40f));
+            SpeedLimitHighway.Index = SpeedLimitHighway.Items.FindIndex(x => (float) x == Config.GetValue("Configuration", "SpeedLimitHighway", 90f));
+            SpeedFactor.Index = SpeedFactor.Items.FindIndex(x => (float) x == Config.GetValue("Configuration", "SpeedFactor", 1f));
+            SpeedUnits.Index = SpeedUnits.Items.FindIndex(x => (string) x == Config.GetValue("Configuration", "SpeedUnits", "KMH"));
+            OpenMenu = Config.GetValue("Configuration", "MenuKey", Keys.B);
 
-            Interval = 10;
             Tick += OnTick;
             KeyUp += OnKeyUp;
         }
 
         void OnTick(object sender, EventArgs e) {
-            if (menuPool != null && menuPool.IsAnyMenuOpen()) {
-                menuPool.ProcessMenus();
+            if (MenuPool != null && MenuPool.IsAnyMenuOpen()) {
+                MenuPool.ProcessMenus();
             }
 
-            if (period < 4) {
-                period++;
+            if (Period < 4) {
+                Period++;
                 return;
             } else {
-                period = 0;
+                Period = 0;
             }
 
-            if (!enabled.Checked || Game.Player.WantedLevel > 0 || !Game.Player.Character.IsInVehicle()) return;
+            if (!Enabled.Checked || Game.Player.WantedLevel > 0 || !Game.Player.Character.IsInVehicle()) return;
 
             Vehicle currentVehicle = Game.Player.Character.CurrentVehicle;
-            float currentSpeed = ((string) speedUnits.Items[speedUnits.Index] == "KMH") ? ToKMH(currentVehicle.Speed) : ToMPH(currentVehicle.Speed);
+            float currentSpeed = ((string) SpeedUnits.Items[SpeedUnits.Index] == "KMH") ? ToKMH(currentVehicle.Speed) : ToMPH(currentVehicle.Speed);
             int timeSinceAgainstTraffic = Function.Call<int>(Hash.GET_TIME_SINCE_PLAYER_DROVE_AGAINST_TRAFFIC, Game.Player);
             int timeSinceDroveOnPavement = Function.Call<int>(Hash.GET_TIME_SINCE_PLAYER_DROVE_ON_PAVEMENT, Game.Player);
             int timeSinceHitPed = Function.Call<int>(Hash.GET_TIME_SINCE_PLAYER_HIT_PED, Game.Player);
             int timeHSinceitVehicle = Function.Call<int>(Hash.GET_TIME_SINCE_PLAYER_HIT_VEHICLE, Game.Player);
 
             if (IsRunningOnRedLight(currentVehicle)) {
-                redLightCount++;
+                RedLightCount++;
             } else {
-                redLightCount = 0;
+                RedLightCount = 0;
             }
 
             if (timeSinceAgainstTraffic == 0) {
-                againstTrafficCount++;
+                AgainstTrafficCount++;
             } else {
-                againstTrafficCount = 0;
+                AgainstTrafficCount = 0;
             }
             if (timeSinceDroveOnPavement == 0) {
-                driveOnPavementCount++;
+                DriveOnPavementCount++;
             } else {
-                driveOnPavementCount = 0;
+                DriveOnPavementCount = 0;
             }
-            if (currentSpeed > (float) speedLimit.Items[speedLimit.Index]) {
-                overspeedCount++;
+            if (currentSpeed > (float) SpeedLimit.Items[SpeedLimit.Index]) {
+                OverspeedCount++;
             } else {
-                overspeedCount = 0;
+                OverspeedCount = 0;
             }
-            if (currentSpeed > (float) speedLimitHighway.Items[speedLimitHighway.Index]) {
-                overspeedHighwayCount++;
+            if (currentSpeed > (float) SpeedLimitHighway.Items[SpeedLimitHighway.Index]) {
+                OverspeedHighwayCount++;
             } else {
-                overspeedHighwayCount = 0;
+                OverspeedHighwayCount = 0;
             }
 
-            // UI.ShowSubtitle("No Arrest " + redLightCount.ToString());
+            // UI.ShowSubtitle("No Arrest " + RedLightCount.ToString());
 
-            if (againstTrafficCount < 16 && driveOnPavementCount < 16 &&
-                overspeedCount < 16 && overspeedHighwayCount < 16 &&
+            if (AgainstTrafficCount < 16 && DriveOnPavementCount < 16 &&
+                OverspeedCount < 16 && OverspeedHighwayCount < 16 &&
                 (timeSinceHitPed < 0 || timeSinceHitPed > 200) &&
                 (timeHSinceitVehicle < 0 || timeHSinceitVehicle > 200) &&
-                (redLightCount == 0)
+                (RedLightCount == 0)
             ) {
                 return;
             }
 
-            // UI.ShowSubtitle("Arrest " + redLightCount.ToString());
+            // UI.ShowSubtitle("Arrest " + RedLightCount.ToString());
 
             List<PedHash> allowedCops = new List<PedHash> {
                 (PedHash) PedHash.Cop01SFY,
@@ -118,91 +117,18 @@ namespace BetterTrafficLaws {
                 (PedHash) PedHash.Ranger01SFY,
                 (PedHash) PedHash.Ranger01SMY
             };
-            if (overspeedHighwayCount > 16) {
+            if (OverspeedHighwayCount > 16) {
                 allowedCops.AddRange(new List<PedHash> {
                     (PedHash) PedHash.Hwaycop01SMY
                 });
             }
-            Ped[] nearbyPeds = World.GetNearbyPeds(Game.Player.Character.Position, (float) copsDistance.Items[copsDistance.Index]);
+            Ped[] nearbyPeds = World.GetNearbyPeds(Game.Player.Character.Position, (float) CopsDistance.Items[CopsDistance.Index]);
             foreach (Ped p in nearbyPeds) {
                 if (!allowedCops.Contains((PedHash) p.Model.Hash)) continue;
                 if (Math.Abs(Game.Player.Character.Position.Z - p.Position.Z) < 20f) {
-                    Game.Player.WantedLevel = (int) starsToAdd.Items[starsToAdd.Index];
+                    Game.Player.WantedLevel = (int) StarsToAdd.Items[StarsToAdd.Index];
                 }
             }
-        }
-
-        void OnKeyUp(object sender, KeyEventArgs e) {
-            if (e.KeyCode == openMenu && !menuPool.IsAnyMenuOpen()) {
-                mainMenu.Visible = !mainMenu.Visible;
-            }
-        }
-
-        void OnCheckBoxChange(UIMenu sender, UIMenuCheckboxItem checkboxItem, bool Checked) {
-            if (checkboxItem == enabled) {
-                string value = Checked ? "True" : "False";
-                config.SetValue("Configuration", "Enabled", value);
-            }
-
-            config.Save();
-        }
-
-        void OnListChange(UIMenu sender, UIMenuListItem listItem, int newIndex) {
-            if (listItem == copsDistance) {
-                config.SetValue("Configuration", "Cops visibility distance", (float) copsDistance.Items[copsDistance.Index]);
-            }
-            if (listItem == starsToAdd) {
-                config.SetValue("Configuration", "Stars to add", (int) starsToAdd.Items[starsToAdd.Index]);
-            }
-            if (listItem == speedLimit) {
-                config.SetValue("Configuration", "Speed limit", (float) speedLimit.Items[speedLimit.Index]);
-            }
-            if (listItem == speedLimitHighway) {
-                config.SetValue("Configuration", "Speed limit highway", (float) speedLimitHighway.Items[speedLimitHighway.Index]);
-            }
-            if (listItem == speedFactor) {
-                config.SetValue("Configuration", "Speed factor", (float) speedFactor.Items[speedFactor.Index]);
-            }
-            if (listItem == speedUnits) {
-                config.SetValue("Configuration", "Speed units", (string) speedUnits.Items[speedUnits.Index]);
-            }
-
-            config.Save();
-        }
-
-        void MainMenu() {
-            menuPool = new MenuPool();
-            mainMenu = new UIMenu("Better Traffic Laws", "Version 1.2.0");
-            menuPool.Add(mainMenu);
-
-            enabled = new UIMenuCheckboxItem("Enabled", true);
-            mainMenu.AddItem(enabled);
-
-            copsDistance = new UIMenuListItem("Cops visibility distance",
-                new List<object> { 20f, 25f, 30f, 35f, 40f, 45f, 50f, 55f, 60f, 65f, 70f, 75f, 80f, 85f, 90f, 95f, 100f }, 0);
-            mainMenu.AddItem(copsDistance);
-
-            starsToAdd = new UIMenuListItem("Stars to add",
-                new List<object> { 1, 2, 3, 4, 5 }, 0);
-            mainMenu.AddItem(starsToAdd);
-
-            speedLimit = new UIMenuListItem("Speed limit",
-                new List<object> { 10f, 15f, 20f, 25f, 30f, 35f, 40f, 45f, 50f, 55f, 60f, 65f, 70f, 75f, 80f, 85f, 90f, 95f, 100f, 105f, 110f, 115f, 120f, 125f, 130f }, 0);
-            mainMenu.AddItem(speedLimit);
-
-            speedLimitHighway = new UIMenuListItem("Speed limit highway",
-                new List<object> { 10f, 15f, 20f, 25f, 30f, 35f, 40f, 45f, 50f, 55f, 60f, 65f, 70f, 75f, 80f, 85f, 90f, 95f, 100f, 105f, 110f, 115f, 120f, 125f, 130f }, 0);
-            mainMenu.AddItem(speedLimitHighway);
-
-            speedFactor = new UIMenuListItem("Speed factor",
-                new List<object> { 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f, 1.8f, 1.9f, 2f }, 0);
-            mainMenu.AddItem(speedFactor);
-
-            speedUnits = new UIMenuListItem("Speed units", new List<object> { "KMH", "MPH" }, 0);
-            mainMenu.AddItem(speedUnits);
-
-            mainMenu.OnCheckboxChange += OnCheckBoxChange;
-            mainMenu.OnListChange += OnListChange;
         }
 
         bool IsRunningOnRedLight(Vehicle target) {
@@ -245,11 +171,84 @@ namespace BetterTrafficLaws {
         }
 
         public float ToMPH(float speed) {
-            return speed * 2.23694f * (float) speedFactor.Items[speedFactor.Index];
+            return speed * 2.23694f * (float) SpeedFactor.Items[SpeedFactor.Index];
         }
 
         public float ToKMH(float speed) {
-            return speed * 3.6f * (float) speedFactor.Items[speedFactor.Index];
+            return speed * 3.6f * (float) SpeedFactor.Items[SpeedFactor.Index];
+        }
+
+        void OnKeyUp(object sender, KeyEventArgs e) {
+            if (e.KeyCode == OpenMenu && !MenuPool.IsAnyMenuOpen()) {
+                MainMenu.Visible = !MainMenu.Visible;
+            }
+        }
+
+        void OnCheckBoxChange(UIMenu sender, UIMenuCheckboxItem checkboxItem, bool Checked) {
+            if (checkboxItem == Enabled) {
+                string value = Checked ? "True" : "False";
+                Config.SetValue("Configuration", "Enabled", value);
+            }
+
+            Config.Save();
+        }
+
+        void OnListChange(UIMenu sender, UIMenuListItem listItem, int newIndex) {
+            if (listItem == CopsDistance) {
+                Config.SetValue("Configuration", "Cops visibility distance", (float) CopsDistance.Items[CopsDistance.Index]);
+            }
+            if (listItem == StarsToAdd) {
+                Config.SetValue("Configuration", "Stars to add", (int) StarsToAdd.Items[StarsToAdd.Index]);
+            }
+            if (listItem == SpeedLimit) {
+                Config.SetValue("Configuration", "Speed limit", (float) SpeedLimit.Items[SpeedLimit.Index]);
+            }
+            if (listItem == SpeedLimitHighway) {
+                Config.SetValue("Configuration", "Speed limit highway", (float) SpeedLimitHighway.Items[SpeedLimitHighway.Index]);
+            }
+            if (listItem == SpeedFactor) {
+                Config.SetValue("Configuration", "Speed factor", (float) SpeedFactor.Items[SpeedFactor.Index]);
+            }
+            if (listItem == SpeedUnits) {
+                Config.SetValue("Configuration", "Speed units", (string) SpeedUnits.Items[SpeedUnits.Index]);
+            }
+
+            Config.Save();
+        }
+
+        void MainMenuInit() {
+            MenuPool = new MenuPool();
+            MainMenu = new UIMenu("Better Traffic Laws", "Version 1.2.0");
+            MenuPool.Add(MainMenu);
+
+            Enabled = new UIMenuCheckboxItem("Enabled", true);
+            MainMenu.AddItem(Enabled);
+
+            CopsDistance = new UIMenuListItem("Cops visibility distance",
+                new List<object> { 20f, 25f, 30f, 35f, 40f, 45f, 50f, 55f, 60f, 65f, 70f, 75f, 80f, 85f, 90f, 95f, 100f }, 0);
+            MainMenu.AddItem(CopsDistance);
+
+            StarsToAdd = new UIMenuListItem("Stars to add",
+                new List<object> { 1, 2, 3, 4, 5 }, 0);
+            MainMenu.AddItem(StarsToAdd);
+
+            SpeedLimit = new UIMenuListItem("Speed limit",
+                new List<object> { 10f, 15f, 20f, 25f, 30f, 35f, 40f, 45f, 50f, 55f, 60f, 65f, 70f, 75f, 80f, 85f, 90f, 95f, 100f, 105f, 110f, 115f, 120f, 125f, 130f }, 0);
+            MainMenu.AddItem(SpeedLimit);
+
+            SpeedLimitHighway = new UIMenuListItem("Speed limit highway",
+                new List<object> { 10f, 15f, 20f, 25f, 30f, 35f, 40f, 45f, 50f, 55f, 60f, 65f, 70f, 75f, 80f, 85f, 90f, 95f, 100f, 105f, 110f, 115f, 120f, 125f, 130f }, 0);
+            MainMenu.AddItem(SpeedLimitHighway);
+
+            SpeedFactor = new UIMenuListItem("Speed factor",
+                new List<object> { 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f, 1.8f, 1.9f, 2f }, 0);
+            MainMenu.AddItem(SpeedFactor);
+
+            SpeedUnits = new UIMenuListItem("Speed units", new List<object> { "KMH", "MPH" }, 0);
+            MainMenu.AddItem(SpeedUnits);
+
+            MainMenu.OnCheckboxChange += OnCheckBoxChange;
+            MainMenu.OnListChange += OnListChange;
         }
     }
 }
